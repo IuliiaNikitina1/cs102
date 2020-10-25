015585f10ea468d1a9e8641b5375e7a5ed510ae7
IuliiaNikitina1 <iuliia.nkt@mail.ru>
2020-10-25 18:22:09 +0300

    Trying

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,29 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    key = ""
+    if len(keyword) > 0:
+        while len(key) < len(plaintext): 
+            key += keyword
+        a = 0
+        for i in plaintext:
+            if i.isalpha():
+                lcFlag = i.islower()
+                if lcFlag:
+                    if (ord(i) + ord(key[a]) - ord("a")) <= ord("z"):
+                        ciphertext += chr(ord(i) + ord(key[a]) - ord("a"))
+                    else:
+                        ciphertext += chr(ord(i) + ord(key[a]) - ord("z") - 1)
+                else:
+                    if (ord(i) + ord(key[a]) - ord("A")) <= ord("Z"):
+                        ciphertext += chr(ord(i) + ord(key[a]) - ord("A"))
+                    else:
+                        ciphertext += chr(ord(i) + ord(key[a]) - ord("Z") - 1)
+            else:
+                ciphertext += i
+            a += 1
+    else:
+        ciphertext = plaintext
     return ciphertext
 
 
@@ -26,5 +48,30 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    key = ""
+    if len(keyword) > 0:
+        while len(key) < len(ciphertext):
+            key += keyword
+        a = 0
+        for i in ciphertext:
+            if i.isalpha():
+                lcFlag = i.islower()
+                if lcFlag:
+                    if ord(i) - (ord(key[a]) - ord("a")) >= ord("a"): 
+                        plaintext += chr(ord(i) - ord(key[a]) + ord("a"))
+                    else:
+                        plaintext += chr(ord(i) - ord(key[a]) + ord("z") + 1)
+                else:
+                    # ord(i) - keyLen
+                    # keylen = ord(key[a]) - ord("A")
+                    if ord(i) - (ord(key[a]) - ord("A")) >= ord("A"): 
+                        plaintext += chr(ord(i) - ord(key[a]) + ord("A"))
+                    else:
+                        plaintext += chr(ord(i) - ord(key[a]) + ord("Z") + 1)
+ 
+            else:
+                plaintext += i
+            a += 1
+    else:
+        plaintext = ciphertext
     return plaintext
